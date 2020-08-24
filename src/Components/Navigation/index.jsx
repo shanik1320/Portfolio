8c31648aa06f7b3c34b89c9a05fcf812eee3fa57
shanik hernandez <shaniik.her93@gmail.com>
2020-08-24 11:32:51 -0400

    changed name on links

--- src/Components/Navigation/index.jsx
+++ src/Components/Navigation/index.jsx
@@ -1,8 +1,24 @@
 import React from "react";
+import { Navbar, Nav } from "react-bootstrap";
+import "./style.css";
 
-function Navigation (){
+
+function Navigation() {
     return (
         <div>
+
+            <Navbar bg="light" variant="light">
+                <Navbar.Brand href="#home">Shanik Hernandez</Navbar.Brand>
+                <Nav className="mr-auto"  >
+                    <Nav.Link href="#home">About</Nav.Link>
+                    <Nav.Link href="#projects">Projects</Nav.Link>
+                    <Nav.Link href="#contact">Contact Me</Nav.Link>
+                </Nav>
+                {/* <Form inline>
+                    <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+                    <Button variant="outline-primary">Search</Button>
+                </Form> */}
+            </Navbar>
 
         </div>
     )
