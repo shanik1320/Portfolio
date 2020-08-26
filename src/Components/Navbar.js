91712562652fc468fcb692690e9d5cf0c9ad423a
shanik hernandez <shaniik.her93@gmail.com>
2020-08-26 10:35:24 -0400

    added props to navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -3,20 +3,35 @@ import Navitem from './Navitem';
 // We'll code Navitem.js later for now let's focus on Navbar.js
 // “Navitem” is the sub-component.
 class Navbar extends Component {
-   
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            'NavItemActive': ''
+        }
+    }
+    activeitem = (x) => {
+        if (this.state.NavItemActive.length > 0) {
+            document.getElementById(this.state.NavItemActive).classList.remove('active');
+        }
+        this.setState({ 'NavItemId': x }, () => {
+            document.getElementById(this.state.NavItemActive).classList.add('active');
+        });
+    }
+
     render() {
         return (
             <nav>
-            <ul>
-            <Navitem item="Home" tolink="/" ></Navitem>
-            <Navitem item="About" tolink="/about" ></Navitem>
-            <Navitem item="Education" tolink="/education"></Navitem>
-            <Navitem item="Skills" tolink="/skills"></Navitem>
-            <Navitem item="Contact" tolink="/contact"></Navitem>
-            </ul>
+                <ul>
+                    <Navitem item="Home" tolink="/" ></Navitem>
+                    <Navitem item="About" tolink="/about" ></Navitem>
+                    <Navitem item="Education" tolink="/education"></Navitem>
+                    <Navitem item="Skills" tolink="/skills"></Navitem>
+                    <Navitem item="Contact" tolink="/contact"></Navitem>
+                </ul>
             </nav>
-            )
-        }
+        )
     }
-    
+}
+
 export default Navbar
