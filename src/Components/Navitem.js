b80c345b458c271a58cf3a5d93a971be1243f502
shanik hernandez <shaniik.her93@gmail.com>
2020-08-26 10:33:17 -0400

    links in navitem

--- src/Components/Navitem.js
+++ src/Components/Navitem.js
@@ -5,9 +5,8 @@ import { Link } from "react-router-dom";
 class Navitem extends Component {
 render() {
 return (
-<li>
-<Link to={this.props.tolink} >
-{this.props.item}
+<li id={this.props.item} >
+<Link to={this.props.tolink} onClick={this.props.activec.bind(this,this.props.item)}>{this.props.item}
 </Link>
 </li>
 )
