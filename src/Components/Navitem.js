6decc1f718b3e94ad8c64a9401e8b01bde20117a
shanik hernandez <shaniik.her93@gmail.com>
2020-08-26 12:00:46 -0400

    navitem not responding

--- src/Components/Navitem.js
+++ src/Components/Navitem.js
@@ -3,13 +3,15 @@ import { Link } from "react-router-dom";
 
 
 class Navitem extends Component {
-render() {
-return (
-<li id={this.props.item} >
-<Link to={this.props.tolink} onClick={this.props.activec.bind(this,this.props.item)}>{this.props.item}
-</Link>
-</li>
-)
-}
+    render() {
+        return (
+            <li id={this.props.item} >
+                <Link to={this.props.tolink}
+                    onClick={this.props.activec.bind (this, this.props.item)}>
+                    {this.props.item}
+                </Link>
+            </li>
+        )
+    }
 }
 export default Navitem
