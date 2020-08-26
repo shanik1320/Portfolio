54003772983016ab4d54dd091c16c8f59dccd37f
shanik hernandez <shaniik.her93@gmail.com>
2020-08-26 11:48:58 -0400

    fixed home.js

--- src/Contents/Home.js
+++ src/Contents/Home.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react';
-//importing the picture as "profilepic".
-// import profilepic from '../img/naafi_photo.png';
 import ReactTypingEffect from 'react-typing-effect';
+// import profilepic from '../img/naafi_photo.png';
 
 
 
-class XYZ extends Component {
-    render() {
-        return (
-            {/* <img src={profilepic} className="profilepic"></img> */ }
-
-            < ReactTypingEffect className = "typingeffect" text = { ['I am Abdul Wahid Naafi', 'I am a web developer']} speed = { 100} eraseDelay = { 700} />
+class Home extends Component {
+render() {
+return (
+<div className="condiv home">
+{/* <img src={profilepic} className="profilepic"></img> */}
+<ReactTypingEffect className="typingeffect" text={['I am Abdul Wahid Naafi','I am a web developer']} speed={100} eraseDelay={700}/>
+</div>
 )
-    }
 }
-export default XYZ
+}
+export default Home
