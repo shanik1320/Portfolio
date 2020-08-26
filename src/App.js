62c185d72b9e70f48fe991f23413340bf9745192
shanik hernandez <shaniik.her93@gmail.com>
2020-08-26 12:33:55 -0400

    routed about to app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
 import {
-BrowserRouter as Router,
-Route,
+  BrowserRouter as Router,
+  Route,
 } from "react-router-dom";
 import Navbar from './Components/Navbar';
 import Home from './Contents/Home';
+import About from './Contents/About';
 
 
 function App() {
@@ -13,9 +14,15 @@ function App() {
     <Router>
       <div className="App">
         <Navbar />
+
         <Route exact path="/">
-          <Home/>
+          <Home />
         </Route>
+
+        <Route path="/about">
+          <About />
+        </Route>
+
       </div>
     </Router>
   )
