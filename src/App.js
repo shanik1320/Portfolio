be7c29e2e9542cc7e94d562d597db211deb4ba6e
shanik hernandez <shaniik.her93@gmail.com>
2020-08-26 11:37:32 -0400

    imported navbar to app.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,19 @@
 import React from 'react';
 import './App.css';
+import {
+BrowserRouter as Router,
+Route,
+} from "react-router-dom";
+import Navbar from './Components/Navbar';
 
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Navbar />
+      </div>
+    </Router>
+  )
+}
+export default App;
