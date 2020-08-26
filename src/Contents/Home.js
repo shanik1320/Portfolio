054dd20e86f9653e796fae7e54b53998d03fe659
shanik hernandez <shaniik.her93@gmail.com>
2020-08-26 11:46:45 -0400

    installed reacttypingeffect

--- src/Contents/Home.js
+++ src/Contents/Home.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 //importing the picture as "profilepic".
 // import profilepic from '../img/naafi_photo.png';
+import ReactTypingEffect from 'react-typing-effect';
 
 
 
 class XYZ extends Component {
-render() {
-return (
-{/* <img src={profilepic} className="profilepic"></img> */}
+    render() {
+        return (
+            {/* <img src={profilepic} className="profilepic"></img> */ }
+
+            < ReactTypingEffect className = "typingeffect" text = { ['I am Abdul Wahid Naafi', 'I am a web developer']} speed = { 100} eraseDelay = { 700} />
 )
-}
+    }
 }
 export default XYZ
