1d1d6fdc386c1c6478a65579f1cc3e3a18dafa61
shanik hernandez <shaniik.her93@gmail.com>
2020-08-26 11:52:00 -0400

    working on social.js

--- src/Contents/Home.js
+++ src/Contents/Home.js
@@ -5,13 +5,13 @@ import ReactTypingEffect from 'react-typing-effect';
 
 
 class Home extends Component {
-render() {
-return (
-<div className="condiv home">
-{/* <img src={profilepic} className="profilepic"></img> */}
-<ReactTypingEffect className="typingeffect" text={['I am Abdul Wahid Naafi','I am a web developer']} speed={100} eraseDelay={700}/>
-</div>
-)
-}
+    render() {
+        return (
+            <div className="condiv home">
+                {/* <img src={profilepic} className="profilepic"></img> */}
+                <ReactTypingEffect className="typingeffect" text={['I am Abdul Wahid Naafi', 'I am a web developer']} speed={100} eraseDelay={700} />
+            </div>
+        )
+    }
 }
 export default Home
