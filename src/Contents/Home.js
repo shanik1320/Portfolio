3b179e5ec5449ad411e5e3d2b04214bed6cf0237
shanik hernandez <shaniik.her93@gmail.com>
2020-08-26 11:41:33 -0400

    working on the home.js

---
+++ src/Contents/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+//importing the picture as "profilepic".
+// import profilepic from '../img/naafi_photo.png';
+
+
+
+class XYZ extends Component {
+render() {
+return (
+{/* <img src={profilepic} className="profilepic"></img> */}
+)
+}
+}
+export default XYZ
