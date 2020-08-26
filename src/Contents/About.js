0fc34858d41224908684f612c430e12f286dfcd1
shanik hernandez <shaniik.her93@gmail.com>
2020-08-26 12:29:08 -0400

    added personal information about me

---
+++ src/Contents/About.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+
+class About extends Component {
+    render() {
+        return (
+            <div className="condiv">
+                <h1 className="subtopic">About Me</h1>
+                <h4>Hey there,</h4>
+                <h1>I'm Shanik Hernandez</h1>
+                <h3>Front-End Web <u>Developer</u> </h3>
+                <br></br>
+                <p>I have always had a passion for technology but it was recently when
+                    I decided I want it to purse it and make something good out of it.
+                    Web development has opened my eyes to many things and has challenged me
+                    so much. Coding is my new life and I cant wait to see what it will bring.
+</p>
+            </div>
+        )
+    }
+}
+export default About
