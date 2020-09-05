caed0d61c902f886ab2b1143d01c3b864d6907cc
shanik hernandez <shaniik.her93@gmail.com>
2020-09-04 21:14:05 -0400

    added projects

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -24,9 +24,11 @@ class Navbar extends Component {
             <ul>
             <Navitem item="Home" tolink="/"  activec={this.activeitem}></Navitem>
             <Navitem item="About" tolink="/about"  activec={this.activeitem}></Navitem>
-            <Navitem item="Education" tolink="/education"  activec={this.activeitem}></Navitem>
+            {/* <Navitem item="Education" tolink="/education"  activec={this.activeitem}></Navitem> */}
             <Navitem item="Skills" tolink="/skills"  activec={this.activeitem}></Navitem>
+            <Navitem item="Projects" tolink="/projects"  activec={this.activeitem}></Navitem>
             <Navitem item="Contact" tolink="/contact"  activec={this.activeitem}></Navitem>
+            
             </ul>
             </nav>
             )
